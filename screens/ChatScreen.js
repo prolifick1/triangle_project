5dcf9e4f8d9855193b9c3b9e96f5ce97d946e596
prolifick1 <69706285+prolifick1@users.noreply.github.com>
2022-09-17 15:18:54 -0500

    nav between homepage and chat, wip: login

---
+++ screens/ChatScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ChatScreen = () => {
+  return (
+    <View>
+      <Text>I am the CHAT screen, blah blah blah</Text>
+    </View>
+  )
+}
+
+export default ChatScreen;
+
